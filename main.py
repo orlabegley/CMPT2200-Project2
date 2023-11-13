bed490f04ac3fbe39c7c1462505c3f61ed2dff43
Orla Begley <115444439+orlabegley@users.noreply.github.com>
2023-11-12 23:59:22 -0800

    Actual & Target temperature boxes implemented.

--- main.py
+++ main.py
@@ -5,61 +5,35 @@ from tkinter.messagebox import showinfo
 class MyGui:
     def __init__(self):
         self.myWindow = Tk()
-        self.myWindow.geometry("300x300+100+100")
-        self.myWindow.title("Bro fix your broke ass car!")
-        self.frame = Frame(self.myWindow, width=300, height=300)
+        self.myWindow.geometry("500x300+100+100")
+        self.myWindow.title("Temperature Checker")
+        self.frame = Frame(self.myWindow, width=500, height=300, background="white")
         self.frame.place(x=10, y=10)
-        self.choice1 = IntVar()
-        self.choice2 = IntVar()
-        self.choice3 = IntVar()
-        self.choice4 = IntVar()
-        self.totalText = StringVar()
-        self.choice1.set(0)
-        self.choice2.set(0)
-        self.choice3.set(0)
-        self.choice4.set(0)
 
-        self.total = 0
+        self.actualTemp = IntVar()
+        self.actualTemp.set(20)
+        self.targetTemp = IntVar()
+        self.targetTemp.set(20)
 
-        self.cb1 = Checkbutton(self.frame, text="Oil Change", font=14, variable=self.choice1, command=self.do_this)
-        self.cb1.place(x=20, y=40)
+        # Actual Temperature
+        self.label = Label(self.frame, text="Actual temperature: ", background="white", foreground="black", font=('Arial', 20))
+        self.label.place(x=10, y=40)
+        self.entry = Entry(self.frame, textvariable=self.actualTemp)
+        self.entry.place(x=280, y=40)
+        #Target Temperature
+        self.label = Label(self.frame, text="Target temperature: ", background="white", foreground="black", font=('Arial', 20))
+        self.label.place(x=10, y=100)
 
-        self.cb2 = Checkbutton(self.frame, text="Inspection", font=14, variable=self.choice2, command=self.do_this)
-        self.cb2.place(x=20, y=70)
+        self.entry = Entry(self.frame, textvariable=self.targetTemp)
+        self.entry.place(x=280, y=100)
 
-        self.cb3 = Checkbutton(self.frame, text="Muffler replacement", font=14, variable=self.choice3,
-                               command=self.do_this)
-        self.cb3.place(x=20, y=110)
+        self.submitButton = Button(self.frame, text="Submit", command=self.submit)
 
-        self.cb4 = Checkbutton(self.frame, text="Tire Rotation", font=14, variable=self.choice4, command=self.do_this)
-        self.cb4.place(x=20, y=140)
-
-        self.totalChargeButton = Button(self.frame, text="Calculate the total", font=14, command=self.do_this)
-        self.totalChargeButton.place(x=20, y=170)
-
-        self.totalText.set("Total Charge: ")
-
-        self.totalLabel = Label(self.frame, textvariable=self.totalText, font=14)
-        self.totalLabel.place(x=20, y=200)
         mainloop()
 
-    def do_this(self):
-        self.total = 0
-        print(self.choice1)
-        print(dir(self.choice1))
+    def submit(self):
 
-        print(self.choice2.get())
-        print(self.choice1.get())
-        if self.choice1.get():
-            self.total += 35
-        if self.choice2.get():
-            self.total += 50
-        if self.choice3.get():
-            self.total += 100
-        if self.choice4.get():
-            self.total += 20
-        self.totalText.set("Total Charge: " + str(self.total))
-        showinfo("Total", "Total Charge: " + str(self.total))
+
 
 
 if __name__ == "__main__":
